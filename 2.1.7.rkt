554185dc21a4d76bb61f0d055252ca3ff5acb291
derrekb <45970048+derrekb@users.noreply.github.com>
2020-02-02 18:02:00 -0500

    Update 2.1.7.rkt

--- 2.1.7.rkt
+++ 2.1.7.rkt
@@ -1,6 +1,19 @@
 #lang racket
 (require fsm)
 
+(define m (make-ndfa
+              '(Q-0 Q-1 Q-2 Q-3 Q-4 Q-5)
+              '(a b)
+              'Q-0
+              '(Q-0)
+              `((Q-0 a Q-1)
+                (Q-1 b Q-2)
+                (Q-2 a Q-3)
+                (Q-3 ,EMP Q-0)
+                (Q-0 a Q-4)
+                (Q-4 b Q-5)
+                (Q-5 ,EMP Q-0))))
+
 
 ; fsm fsm [natnum] --> true or (listof words)
 (define (sm-testequiv?  M1 M2 . l)
